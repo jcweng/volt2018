15b6306aa319cd5229aa6cd868c2271875af5626
jcweng <peacecampaign@gmail.com>
2026-05-23 18:28:53 -0400

    Initialize all required submodules for comma.ai installer compatibility

--- tinygrad_repo/extra/usbgpu/tbgpu/installer/notary_tool.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-ditto -c -k --keepParent ./build/Release/TinyGPU.app ./build/Release/TinyGPU.zip
-xcrun notarytool submit ./build/Release/TinyGPU.zip --keychain-profile "hgwJFhdheiIEy82nDN" --wait
